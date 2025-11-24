b5952369efac76ac4aa5726d85d5b1cfefc484b5
John Carmack <johncarmack@me.com>
2025-11-23 22:26:00 -0800

    Run fmt

--- .scripts/ci/get-release-channel.cjs
+++ .scripts/ci/get-release-channel.cjs
@@ -1,19 +1,19 @@
 // @ts-check
 
-const path = require("node:path");
-const fs = require("node:fs");
+const path = require('node:path')
+const fs = require('node:fs')
 
 const fn = () => {
-  const loc = path.resolve(__dirname, "../../.changes/pre.json");
+  const loc = path.resolve(__dirname, '../../.changes/pre.json')
   if (!fs.existsSync(loc)) {
-    return "latest";
+    return 'latest'
   }
-  const data = fs.readFileSync(loc, { encoding: "utf-8" });
-  const obj = JSON.parse(data);
-  const out = obj?.tag ?? "latest";
-  return out.toString();
-};
+  const data = fs.readFileSync(loc, { encoding: 'utf-8' })
+  const obj = JSON.parse(data)
+  const out = obj?.tag ?? 'latest'
+  return out.toString()
+}
 
-console.log(fn());
+console.log(fn())
 
-module.exports = () => fn();
+module.exports = () => fn()
